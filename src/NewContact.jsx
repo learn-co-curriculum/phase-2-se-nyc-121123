2973f3e83188159d0b0fdb2dbf3573eb0a95c32b
Mohammad <moh.hossain98@gmail.com>
2024-01-12 11:58:46 -0500

    updated css and the form

--- src/NewContact.jsx
+++ src/NewContact.jsx
@@ -6,23 +6,21 @@ const NewContact = () => {
   return (
     <div className="new-contact">
       <h2>Add New Contact</h2>
-      <div className="form-group">
-        <label>Name:</label>
+      <form className="form-group">
+        <label>Name: </label>
         <input type="text" name="name" />
-      </div>
-      <div className="form-group">
-        <label>Number:</label>
+
+        <label>Number: </label>
         <input type="text" name="number" />
-      </div>
-      <div className="form-group">
-        <label>Email:</label>
+
+        <label>Email: </label>
         <input type="text" name="email" />
-      </div>
-      <div className="form-group">
-        <label>Address:</label>
-        <input type="text" name="address" />
-      </div>
-      <button>Add Contact</button>
+
+        <label>Address: </label>
+        <input type="text" name="Address" />
+
+        <input className="submit" type="submit" />
+      </form>
     </div>
   );
 };
